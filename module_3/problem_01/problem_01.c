abd99e1340c7452481dd4dac47f79712972ee564
Alexander <165910751+Bukhaa@users.noreply.github.com>
2024-11-22 22:04:39 +0700

    Add files via upload

---
+++ module_3/problem_01/problem_01.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void square(int length){
+	printf("Площадь квадрата со стороной %d: %d\n", length, length * length);
+}
+	
+int main(int argc, char* argv[]) {
+	
+	size_t i;
+	pid_t pid,status;
+	
+	if (argc < 3) {
+        printf("Eror in arguments\n");
+        exit(EXIT_FAILURE);
+    }
+	
+	int count_op = argc / 2;
+	pid = fork();
+	
+	if (-1 == pid) {
+            perror("fork"); 
+            exit(EXIT_FAILURE); 
+    } else if (0 == pid) {
+		
+		for (i = 1; i <=count_op; ++i){
+			
+			square(atoi(argv[i]));
+		}
+		exit(EXIT_SUCCESS);
+    }
+	
+	else{
+		
+		for (i = count_op + 1; i < argc; ++i) 
+			square(atoi(argv[i]));
+		
+		wait(NULL);
+	}
+	
+	exit(EXIT_SUCCESS);
+}
