38cbc0ffdb188c0d39d147e0f06b21085cf92da4
Alexander <165910751+Bukhaa@users.noreply.github.com>
2025-03-01 16:04:50 +0700

    Update problem_01.c

--- module_3/problem_01/problem_01.c
+++ module_3/problem_01/problem_01.c
@@ -5,36 +5,36 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void square(int length){
+void square(int length) {
 	printf("Площадь квадрата со стороной %d: %d\n", length, length * length);
 }
 	
 int main(int argc, char* argv[]) {
 	
 	size_t i;
-	pid_t pid,status;
+	pid_t pid;
 	
 	if (argc < 3) {
-        printf("Eror in arguments\n");
-        exit(EXIT_FAILURE);
-    }
+		
+        	printf("Eror in arguments\n");
+        	exit(EXIT_FAILURE);
+    	}
 	
 	int count_op = argc / 2;
 	pid = fork();
 	
 	if (-1 == pid) {
-            perror("fork"); 
-            exit(EXIT_FAILURE); 
-    } else if (0 == pid) {
+        	perror("fork"); 
+            	exit(EXIT_FAILURE); 
+   	 } else if (0 == pid) {
 		
 		for (i = 1; i <=count_op; ++i){
-			
 			square(atoi(argv[i]));
 		}
 		exit(EXIT_SUCCESS);
-    }
+    	}
 	
-	else{
+	else {
 		
 		for (i = count_op + 1; i < argc; ++i) 
 			square(atoi(argv[i]));
